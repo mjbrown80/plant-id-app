b14cdcb2723f798427201bf7773c5a3af70f730e
Matt Brown <matthewjbrown365@gmail.com>
2024-05-10 15:05:56 -0400

    created searchbar componenet

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -8,17 +8,32 @@
       <p>-Claude Monet</p>
       </div>
     <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'plants' }">Plants</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-    </div>
-    <div>
-      
+      <router-link v-bind:to="{ name: 'home' }" class="nav-link" >Home</router-link>
+      <router-link v-bind:to="{ name: 'plants' }" class="nav-link">Plants</router-link>
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''" class="nav-link">Logout</router-link>
+      <!-- <input type="text" placeholder="Search.." class="nav-search"> -->
+      <search-bar @search="performSearch"/>
     </div>
     </header>
     <router-view />
   </div>
 </template>
+
+<script>
+import SearchBar from './components/SearchBar.vue'
+
+export default {
+  components: {
+    SearchBar
+  },
+  methods: {
+    performSearch(query){
+      console.log('Search query:', query)
+    }
+  }
+}
+</script>
+
 
 <style>
 *{
@@ -51,12 +66,34 @@ body {
   font-size: 2rem;
 }
 #nav {
+  padding: 14px 16px;
   padding: 1rem;
   background-color: #FEDE00;
   border: #0A7029;
   border-style: outset;
 }
+#nav a:hover {
+  background-color: #0A7029;
+  color: black;
+}
+/* #nav a:active {
+  background-color: #0A7029;
+  color: white;
+} */
+.nav-link {
+  font-size: 30px;
+  float: left;
+  padding: 14px 16px;
+}
 #monet-quote{
   color: #0A7029;
+}
+input {
+  float: right;
+  padding: 10px;
+  border: none;
+  margin-top: 8px;
+  margin-right: 16px;
+  font-size: 17px;
 }
 </style>
---
+++ vue/src/components/SearchBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input type="text" v-model="searchQuery" @input="search" placeholder="Search..." class="nav-search" >
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return{
+            searchQuery: ''
+        }
+    },
+    methods: {
+        search(){
+            this.$emit('search', this.searchQuery)
+        }
+    }
+
+}
+</script>
+
+<style>
+.nav-search{
+    padding: 10px;
+    border: none;
+    font-size: 17px;
+}
+
+</style>
