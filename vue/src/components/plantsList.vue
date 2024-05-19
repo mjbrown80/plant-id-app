a26ab393016432c63c6d3153b1fe14ebd7123078
Matt Brown <matthewjbrown365@gmail.com>
2024-05-18 21:57:06 -0400

    still working on button functionallity

--- vue/src/components/plantsList.vue
+++ vue/src/components/plantsList.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div>
     <ul>  
@@ -31,20 +32,25 @@ import plantService from "../services/PlantService";
 
 export default {
   name: "plant-list",
-  data() {
-    return {
-      plantArray: [],
-    };
-  },
-  methods: {},
-  created() {
-    if(!this.$store.state.plantArray.length){
-      plantService.getPlants()
-      .then(response => {
-        this.$store.commit("INIT_PLANTS", response.data)
-      })
+  computed: {
+    plantArray() {
+      return this.$store.state.plantArray
     }
-  },
+  }
+  // data() {
+  //   return {
+  //     plantArray: [],
+  //   };
+  // },
+  // methods: {},
+  // created() {
+  //   if(!this.$store.state.plantArray.length){
+  //     plantService.getPlants()
+  //     .then(response => {
+  //       this.$store.commit("INIT_PLANTS", response.data)
+  //     })
+  //   }
+  // },
   }
 </script>
 
@@ -97,3 +103,126 @@ li {
 
 
 </style>
+
+<!-- <template>
+  <div>
+    <button>hello</button>
+    <ul>
+      <li v-for="plant in allPlants" :key="plant.id">
+        <router-link :to="{ name: 'detail', params: { id: plant.id } }">
+          <div id="card">
+            {{ plant.name }}
+            <img id="thumbnail" :src="plant.url" alt="No Image Available" />
+          </div>
+        </router-link>
+      </li>
+    </ul>
+    <div id="button-container">
+      <button id="previous-plants" @click="getPreviousPlants" :disabled="startingVal === 0">Previous</button>
+      <button id="next-plants" @click="getNextPlants">Next</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState, mapMutations } from 'vuex';
+import plantService from "../services/PlantService";
+
+export default {
+  name: "plant-list",
+  computed: {
+    ...mapState(['plantArray', 'startingVal']),
+    allPlants() {
+      return this.plantArray;
+    }
+  },
+  created() {
+    if (!this.plantArray.length) {
+      console.log('Fetching initial plants');
+      plantService.getPlants().then(response => {
+        this.$store.commit("INIT_PLANTS", response.data);
+        console.log('Initial plants:', response.data);
+      });
+    }
+  },
+  methods: {
+    ...mapMutations(['GET_NEXT_PREVIOUS']),
+    getNextPlants() {
+      let start = this.startingVal + 30;
+      console.log('Fetching next plants from', start);
+      plantService.getNewPlants(start).then(response => {
+        console.log('Next plants:', response.data);
+        this.GET_NEXT_PREVIOUS({ startingVal: start, plantArray: response.data });
+      });
+    },
+    getPreviousPlants() {
+      let start = Math.max(this.startingVal - 30, 0);
+      console.log('Fetching previous plants from', start);
+      plantService.getNewPlants(start).then(response => {
+        console.log('Previous plants:', response.data);
+        this.GET_NEXT_PREVIOUS({ startingVal: start, plantArray: response.data });
+      });
+    }
+  },
+  watch: {
+    plantArray(newVal) {
+      console.log('Updated plantArray:', newVal);
+    },
+    startingVal(newVal) {
+      console.log('Updated startingVal:', newVal);
+    }
+  }
+};
+</script>
+
+<style>
+/* Same CSS as before */
+</style>
+
+<style>
+ul > li {
+  list-style-type: none;
+}
+#thumbnail {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  margin-top: 1rem;
+  margin-bottom: 3rem;
+  display: block;
+}
+#card {
+  width: 300px;
+  height: 300px;
+  border: black;
+  border-style: outset;
+  margin: 1rem;
+  align-content: center;
+  background: linear-gradient(#c8df52, #0a7029);
+}
+li {
+  display: inline-block;
+}
+#button-container {
+  margin-top: 20px;
+}
+#previous-plants, #next-plants {
+  background-color: #0a7029;
+  color: ivory;
+  margin: 20px;
+  padding: 8px 16px;
+  border-radius: 10px;
+  min-height: 30px;
+  min-width: 120px;
+}
+#next-plants:hover {
+  background-color: #C8DF52;
+  color: #0a7029;
+  transition: 0.7s;
+}
+#previous-plants:hover {
+  background-color: #C8DF52;
+  color: #0a7029;
+  transition: 0.7s;
+}
+</style> -->
--- vue/src/views/PlantsView.vue
+++ vue/src/views/PlantsView.vue
@@ -1,3 +1,4 @@
+
 <template>
   <div>
     <search-list v-if="searchResults.length > 0" :plantArray="searchResults"/>
@@ -44,12 +45,12 @@ export default {
     }
   },
   methods: {
-    // getAllPlants(){
-    //   PlantService.getPlants()
-    //   .then(response => {
-    //     this.allPlants = response.data
-    //   })
-    // },
+    getAllPlants(){
+      PlantService.getPlants()
+      .then(response => {
+        this.allPlants = response.data
+      })
+    },
     performSearch(query){
       console.log("Search query:", query)
       PlantService.searchPlants(query)
@@ -74,12 +75,7 @@ export default {
          startingVal: start,
          plantArray: response.data
         })
-        // let values = {
-        //   startingVal: start,
-        //   plantArray: response.data
-        // }
-        // this.$store.commit("GET_NEXT_PREVIOUS", values)
-        // this.allPlants = this.$store.state.plantArray
+      
       })
     },
     getPreviousPlants() {
@@ -90,12 +86,6 @@ export default {
           startingVal: start,
           plantArray: response.data
         })
-        // let values = {
-        //   startingVal: start,
-        //   plantArray: response.data
-        // }
-        // this.$store.commit("GET_NEXT_PREVIOUS", values)
-        // this.allPlants = this.$store.state.plantArray
       })
     }
   },
@@ -105,3 +95,55 @@ export default {
 <style>
 
 </style>
+
+<!-- <template>
+  <div>
+    <search-list v-if="searchResults.length > 0" :plantArray="searchResults"/>
+    <plant-list v-else />
+  </div>
+</template>
+
+<script>
+import PlantList from '../components/plantsList.vue';
+import SearchList from '../components/SearchList.vue';
+import PlantService from '../services/PlantService';
+
+
+export default {
+  components: { 
+    PlantList,
+    SearchList,
+  },
+  data() {
+    return {
+      searchResults: []
+    };
+  },
+  created() {
+    this.performSearch(this.$route.query.search || '');
+  },
+  watch: {
+    '$route.query.search'(newVal) {
+      this.performSearch(newVal || '');
+    }
+  },
+  methods: {
+    performSearch(query) {
+      console.log("Search query:", query);
+      PlantService.searchPlants(query)
+      .then(response => {
+        if (Array.isArray(response.data)){
+          this.searchResults = response.data;
+        } else if (response.data instanceof Object){
+          this.searchResults = [response.data];
+        } else {
+          this.searchResults = [];
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+  }
+};
+</script> -->
